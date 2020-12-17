1f0081d2e714ddd02bd06c74235d5cf515b462bb
swanf <8128390+swanf@user.noreply.gitee.com>
2020-12-17 21:40:59 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+Inspired by @bpceee 's  [oldest](https://github.com/bpceee/oldest) bookmarklet.
+
+Basically does the same thing. Just add a bookmark whose address equals "javascript:" + `<code in src/index.js>` 
---
+++ src/index.js
@@ -0,0 +1,12 @@
+(([_, repo, branch='master']) => {
+  fetch(`https://github.com/${repo}/tree/${branch}`)
+    .then(res => res.text())
+    .then(res => {
+      let mainDocument = new DOMParser().parseFromString(res, 'text/html');
+      let commitCount = mainDocument.evaluate('//span[@class="d-none d-sm-inline"]//strong', mainDocument.body).iterateNext().innerText;
+      commitCount = Number(commitCount.trim().replace(',', ''));
+      let commitId = mainDocument.evaluate('//*[@class="f6 link-gray text-mono ml-2 d-none d-lg-inline"]', mainDocument.body).iterateNext().getAttribute("href").split('/').pop();
+      let url = `https://github.com/${repo}/commits/${branch}?after=${commitId}+${commitCount-10}`;
+      window.location = url;
+  })
+})(window.location.pathname.match(/\/([^\/]+\/[^\/]+)(?:\/(?:tree|commits)\/(.+))?/));
